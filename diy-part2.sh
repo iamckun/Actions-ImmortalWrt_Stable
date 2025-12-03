fccc47a72641ee9b5e2bec895b839d8ef1cd3af2
CKun <65990499+iamckun@users.noreply.github.com>
2025-12-03 17:26:37 +0800

    更多的包

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,21 +11,21 @@
 #
 
 # 更改默认IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.18.18.5/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #添加软件包
 #OpenClash
-#rm -rf feeds/luci/applications/luci-app-openclash
-#git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 #AdguardHome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #Mihomo
-#git clone -b main --single-branch --filter=blob:none https://github.com/nikkinikki-org/OpenWrt-nikki
-#mv OpenWrt-nikki/luci-app-nikki package/
-#mv OpenWrt-nikki/nikki package/
+git clone -b main --single-branch --filter=blob:none https://github.com/nikkinikki-org/OpenWrt-nikki
+mv OpenWrt-nikki/luci-app-nikki package/
+mv OpenWrt-nikki/nikki package/
 #添加qosmate
-#git clone https://github.com/hudra0/qosmate.git package/qosmate
-#git clone https://github.com/LemonCrab666/luci-app-qosmate.git package/luci-app-qosmate
+git clone https://github.com/hudra0/qosmate.git package/qosmate
+git clone https://github.com/LemonCrab666/luci-app-qosmate.git package/luci-app-qosmate
 
 #修改nginx默认http
 mkdir -p files/etc/config
@@ -71,30 +71,30 @@ EOF
 chmod 0644 files/etc/sysupgrade.conf
 
 #将clash内核、TUN内核、Meta内核编译进目录
-#mkdir -p files/etc/openclash/core
+mkdir -p files/etc/openclash/core
 #Dev内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz | tar -xz -C /tmp
-#mv /tmp/clash files/etc/openclash/core/clash
-#chmod 0755 files/etc/openclash/core/clash
+curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz | tar -xz -C /tmp
+mv /tmp/clash files/etc/openclash/core/clash
+chmod 0755 files/etc/openclash/core/clash
 #tun内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17-13-gdcc8d87.gz | gunzip -c > /tmp/clash_tun
-#mv /tmp/clash_tun files/etc/openclash/core/clash_tun
-#chmod 0755 files/etc/openclash/core/clash_tun
+curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17-13-gdcc8d87.gz | gunzip -c > /tmp/clash_tun
+mv /tmp/clash_tun files/etc/openclash/core/clash_tun
+chmod 0755 files/etc/openclash/core/clash_tun
 #meta内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar -xz -C /tmp
-#mv /tmp/clash files/etc/openclash/core/clash_meta
-#chmod 0755 files/etc/openclash/core/clash_meta
+curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar -xz -C /tmp
+mv /tmp/clash files/etc/openclash/core/clash_meta
+chmod 0755 files/etc/openclash/core/clash_meta
 
 #将AdGuardHome核心文件编译进目录
-#curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest \
-#| grep "browser_download_url.*AdGuardHome_linux_amd64.tar.gz" \
-#| cut -d : -f 2,3 \
-#| tr -d \" \
-#| xargs curl -L -o /tmp/AdGuardHome_linux_amd64.tar.gz && \
-#tar -xzvf /tmp/AdGuardHome_linux_amd64.tar.gz -C /tmp/ --strip-components=1 && \
-#mkdir -p files/usr/bin/AdGuardHome && \
-#mv /tmp/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome/
-#chmod 0755 files/usr/bin/AdGuardHome/AdGuardHome
+curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest \
+| grep "browser_download_url.*AdGuardHome_linux_amd64.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| xargs curl -L -o /tmp/AdGuardHome_linux_amd64.tar.gz && \
+tar -xzvf /tmp/AdGuardHome_linux_amd64.tar.gz -C /tmp/ --strip-components=1 && \
+mkdir -p files/usr/bin/AdGuardHome && \
+mv /tmp/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome/
+chmod 0755 files/usr/bin/AdGuardHome/AdGuardHome
 
 # 修改固件MD5值
 # 生成VerMagic文件
